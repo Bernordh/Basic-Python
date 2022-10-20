a1702dade60e05e62c66612b841751a02cb4ff3d
Sentessh <67072582+Bernordh@users.noreply.github.com>
2022-10-20 00:49:34 -0300

    Delete Horas.py

--- Horas.py
+++
@@ -1,9 +0,0 @@
-tempo = 24720
-tempo1 = 1980
-tempo2 = 2592
-tempof = tempo + tempo1 + tempo2
-tnorm = (tempof / 60) / 60
-thoras = round(((tempof / 60) / 60) - 0.5)
-tmin = tnorm - thoras
-min = round(tmin * 60)
-print(f"Você vai chagar em casa as {thoras} horas e {min} minutos")
